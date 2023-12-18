9acfeaa8f76462b09b3350e0425131c399d175aa
Torsten Dreyer <torsten@t3r.de>
2023-12-18 20:49:25 +0100

    use production config if NODE_ENV is set to production

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
 const ViteExpress = require( "vite-express" );
 
+if( "production" === process.env.NODE_ENV )
+  ViteExpress.config({ mode: "production" });
+
 ViteExpress.listen(
   require('./backend/app.js'),
   port = parseInt(process.env.PORT || '3000', 10), 
